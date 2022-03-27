2e1fc851296bcec1a1f588b5b0b0b275816e6944
RB-Bahry <rabebraja93@gmail.com>
2022-03-27 19:36:51 +0100

    arrow placement

--- src/index.tsx
+++ src/index.tsx
@@ -245,7 +245,7 @@ function ScrollMenu({
       onMouseMove={onMouseMove?.(context)}
     >
       <VisibilityContext.Provider value={context}>
-        {LeftArrow}
+        <div>{LeftArrow}{RightArrow}</div>
         <ScrollContainer
           className={scrollContainerClassName}
           onScroll={scrollHandler}
@@ -259,7 +259,7 @@ function ScrollMenu({
             {children}
           </MenuItems>
         </ScrollContainer>
-        {RightArrow}
+        
       </VisibilityContext.Provider>
     </div>
   );
